eeb596f2d3f35da9882b6ca2812fe2d8a9b4769d
Evgeniy Koveshnikov <koveshnikov.evgeniy@gmail.com>
2019-10-03 00:28:59 +0700

    initial commit

---
+++ src/main/java/com/partymakers/shareparty/SharePartyApplication.java
@@ -0,0 +1,12 @@
+package com.partymakers.shareparty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharePartyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharePartyApplication.class, args);
+	}
+}
