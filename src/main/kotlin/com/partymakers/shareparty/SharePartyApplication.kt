cbd10355382e1d1550df03809016641cb1624ce0
e.koveshnikov <e.koveshnikov@vk.team>
2025-07-02 13:56:52 +0700

    Перевел проект на kotlin

---
+++ src/main/kotlin/com/partymakers/shareparty/SharePartyApplication.kt
@@ -0,0 +1,11 @@
+package com.partymakers.shareparty
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SharePartyApplication
+
+fun main() {
+    runApplication<SharePartyApplication>()
+}
