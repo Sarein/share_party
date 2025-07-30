95656f9aa07f5fe3772396d1c9541669a447c187
e.koveshnikov <e.koveshnikov@vk.team>
2025-07-30 15:03:11 +0700

    Переделал структуру БД и контроллера

---
+++ src/main/kotlin/com/partymakers/shareparty/party/presentation/mapper/ExpenseMapper.kt
@@ -0,0 +1,17 @@
+package com.partymakers.shareparty.party.presentation.mapper
+
+import com.partymakers.shareparty.party.domain.entity.Expense
+import com.partymakers.shareparty.party.presentation.dto.ExpenseDto
+import org.springframework.stereotype.Component
+
+@Component
+internal class ExpenseMapper {
+
+    fun toModel(dto: ExpenseDto): Expense =
+        Expense(
+            id = dto.id?.toLongOrNull(),
+            name = dto.name,
+            cost = dto.cost,
+            count = dto.count
+        )
+}
