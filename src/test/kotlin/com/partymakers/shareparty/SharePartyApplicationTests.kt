961c5ad7d8870d812325e7492ec03fd0dac5ef43
ntnsmirnov <ntnsmirnov@gmail.com>
2025-08-10 14:23:59 +0300

    Переписал на котлин SharePartyApplicationTests

---
+++ src/test/kotlin/com/partymakers/shareparty/SharePartyApplicationTests.kt
@@ -0,0 +1,21 @@
+package com.partymakers.shareparty
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+
+@SpringBootTest
+@ActiveProfiles("test")
+class SharePartyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+        // Проверяем, что Spring контекст загружается успешно
+    }
+
+    @Test
+    fun applicationStarts() {
+        // Проверяем, что приложение запускается
+        assert(true) // Простая проверка, что тест выполняется
+    }
+} 
