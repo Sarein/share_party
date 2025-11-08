33e4ffbfaa0f015670cd4d5bb891c52fe6501416
ntnsmirnov <ntnsmirnov@gmail.com>
2025-11-08 13:10:20 +0300

    починил тесты в SimpleApplicationTest

--- src/test/kotlin/com/partymakers/shareparty/SimpleApplicationTest.kt
+++ src/test/kotlin/com/partymakers/shareparty/SimpleApplicationTest.kt
@@ -7,10 +7,14 @@ import org.springframework.test.context.TestPropertySource
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestPropertySource(
     properties = [
-        "spring.datasource.url=jdbc:postgresql://localhost:5432/shareparty;DB_CLOSE_DELAY=-1",
-        "spring.datasource.driver-class-name=org.postgresql.Driver",
+        "spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE",
+        "spring.datasource.driver-class-name=org.h2.Driver",
+        "spring.datasource.username=sa",
+        "spring.datasource.password=password",
+        "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
         "spring.jpa.hibernate.ddl-auto=create-drop",
-        "spring.jpa.show-sql=true"
+        "spring.jpa.show-sql=true",
+        "spring.liquibase.enabled=false"
     ]
 )
 class SimpleApplicationTest {
